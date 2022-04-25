137d006b7501722caa058f7a3f32094b992e03f9
Rafał Dziwiński <rafal.dziwinski@korbank.pl>
2022-04-25 23:58:20 +0200

    some more stuff

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+env = environ.Env()
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +30,8 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'django-insecure-e#0o^z1&eox@x--mlxnm5pd)$mnmr(b4=%&4rlp%6s5o4=rk@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
 
 ALLOWED_HOSTS = []
 
@@ -78,17 +82,27 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nboard',
+#         'USER': 'django',
+#         'PASSWORD': 'uruyie1kaMae',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nboard',
-        'USER': 'django',
-        'PASSWORD': 'uruyie1kaMae',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': env("ENGINE", 'django.db.backends.postgresql'),
+        'NAME': env("POSTGRES_NAME", 'nboard'),
+        'USER': env("POSTGRES_USER", 'django'),
+        'PASSWORD': env("POSTGRES_PASSOWRD", 'uruyie1kaMae'),
+        'HOST': env("POSTGRES_HOST", '127.0.0.1'),
+        'PORT': env("POSTGRES_PORT", '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+
 
 from django.core.wsgi import get_wsgi_application
 
